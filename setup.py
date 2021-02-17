83395efc10b59a4a4a8601c204a63a07f5790d75
Robert Stein <robert.stein@desy.de>
2021-02-17 16:05:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,9 +17,12 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
     install_requires=[
         "numpy",
         "healpy",
