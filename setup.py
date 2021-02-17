7a79f49e6391d57a6657c2d8e13bd108ffbf20fb
Robert Stein <robert.stein@desy.de>
2021-02-17 16:34:10 +0100

    Force numpy >1.17

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
-        "numpy",
+        "numpy>=1.17.0",
         "healpy",
         "scipy",
         "matplotlib",
