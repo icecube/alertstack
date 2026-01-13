b12446b448aa0ead8e212bdee840086cd9b94c00
G-Sommani <giacomo.sommani@gmail.com>
2026-01-13 04:48:50 -0600

    Update __init__.py for the radio agn analysis

---
+++ examples/radio_agn_neutrino_alert/__init__.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from alertstack.analyse import Analyse
+from alertstack.scramble_catalogues.agn_radio_catalogue import AstrogeoAGNCatalogue, AverageFluxWeightHypothesis
+from alertstack.fixed_catalogues.icecube_neutrino_alerts import CircularisedNeutrinoAlertCatalogue, HealpixNeutrinoAlertCatalogue
+
+agn_cache = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cache/")
+
+agn_analysis = Analyse(
+    AstrogeoAGNCatalogue(),
+    [AverageFluxWeightHypothesis],
+    HealpixNeutrinoAlertCatalogue(),
+    cache_dir=agn_cache,
+    clean_cache=False
+)
+
+def plot_ts(val, sens, disc_3, disc_5, gd, filename="ts_bkg.png"):
+    # plot TS + gamma distribution + sensitivity and disc potential
+    data = np.array(val)
+    plt.hist(data, density=True, bins=50)
+    x_range = np.linspace(min(data), max(data), 100)
+    ylim = plt.gca().get_ylim() 
+    plt.plot(x_range, gd.dist.pdf(x_range))
+    plt.xlabel('TS')
+    plt.axvline(sens, color = 'tab:orange', ls='--', label='Sensitivity')
+    plt.axvline(disc_3, color = 'tab:orange', ls='-.', label='3sigma disc')
+    plt.axvline(disc_5, color = 'tab:orange', ls=':', label='5sigma disc')
+    plt.legend()
+    plt.savefig(os.path.join(agn_cache,filename),dpi=500)
+
+def plot_ts_data(val, gd, ts_data, name=None):
+    # plot TS + gamma distribution + TS of data
+    data = np.array(val)
+    plt.hist(data, density=True, bins=50)
+    x_range = np.linspace(min(data), max(data), 100)
+    ylim = plt.gca().get_ylim() 
+    plt.plot(x_range, gd.dist.pdf(x_range))
+    plt.yscale('log')
+    plt.xlabel('TS')
+    plt.axvline(ts_data, color = 'tab:orange', label='TS with RFC 2022a')
+    plt.legend()
+    if name:
+        plt.savefig(os.path.join(agn_cache, name+'.png'),dpi=500)
+    else:
+        plt.savefig(os.path.join(agn_cache, 'ts_data.png'),dpi=500)
+
