885cc4e459d179e6637340b5b2066f8d3aaa5319
G-Sommani <giacomo.sommani@gmail.com>
2026-02-19 09:18:29 -0600

    Add if __name__ == __main__ to plot_all.py

--- examples/plot_all.py
+++ examples/plot_all.py
@@ -2,16 +2,19 @@ import glob
 import os
 import subprocess
 
-cwd = os.path.dirname(os.path.realpath(__file__))
-dirs = glob.glob(os.path.join(cwd,'*alert/'))
-dirs.sort()
 
-for d in dirs:
-    name_dir = d.split("/")[-2]
-    print(name_dir)
-    plot_script = glob.glob(os.path.join(d, "plot_*"))[0]
-    subprocess.run(
-        f'python {plot_script}',
-        shell=True,
-        executable="/bin/bash"
-    )
+if __name__ == "__main__":
+
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    dirs = glob.glob(os.path.join(cwd,'*alert/'))
+    dirs.sort()
+    
+    for d in dirs:
+        name_dir = d.split("/")[-2]
+        print(name_dir)
+        plot_script = glob.glob(os.path.join(d, "plot_*"))[0]
+        subprocess.run(
+            f'python {plot_script}',
+            shell=True,
+            executable="/bin/bash"
+        )
