a30163d5cd1e4e8466c72b96855cc9469517c729
G-Sommani <giacomo.sommani@gmail.com>
2026-03-23 11:21:44 -0500

    Update Sjoert analysis

--- examples/sjoert_accretion_flares_neutrino_alert/run_sjoert_analysis.py
+++ examples/sjoert_accretion_flares_neutrino_alert/run_sjoert_analysis.py
@@ -10,13 +10,33 @@ from examples.sjoert_accretion_flares_neutrino_alert import (
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Calculate TS distributions')
-    parser.add_argument('--n_trials', type=int, default=500,
-                            help = 'Number of trials')
-    parser.add_argument('--fraction', type=float, default=0.5,
-                            help = 'Maximum fraction of neutrinos to be correlated')
-    parser.add_argument('--n_steps', type=int, default=10, help ='Number of steps')
-    parser.add_argument('--tag', type=str, default="", help ='Additional tag')
+    parser = argparse.ArgumentParser(
+        description='Calculate TS distributions'
+    )
+    parser.add_argument(
+        '--n_trials',
+        type=int,
+        default=500,
+        help = 'Number of trials'
+    )
+    parser.add_argument(
+        '--fraction',
+        type=float,
+        default=0.5,
+        help = 'Maximum fraction of neutrinos to be correlated'
+    )
+    parser.add_argument(
+        '--n_steps', type=int, default=10, help ='Number of steps'
+    )
+    parser.add_argument(
+        '--tag', type=str, default="", help ='Additional tag'
+    )   
+    parser.add_argument(
+        '--run',
+        type=int,
+        default=200000,
+        help ='Last run to consider for the neutrinos'
+    )
     args = parser.parse_args()
     
     '''
@@ -24,6 +44,7 @@ if __name__ == "__main__":
     will be run as background trials
     fraction: Maximum fraction of astrophysical neutrinos to be injected
     n_steps: Number of different injection steps to test, between 0 and fraction.
+    run: Last run to consider for the neutrinos.
     '''
 
     logging.getLogger().setLevel("INFO")
