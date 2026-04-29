e81321004782a334004dd11b94e7bc1a1e4cf561
G-Sommani <giacomo.sommani@gmail.com>
2026-04-29 10:16:27 -0500

    python 3.12

--- setup.py
+++ setup.py
@@ -16,13 +16,11 @@ setuptools.setup(
     url="https://github.com/icecube/alertstack",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.12',
     install_requires=[
         "numpy>=1.17.0,<2",
         "healpy",
